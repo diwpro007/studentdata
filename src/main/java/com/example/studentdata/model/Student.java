98892c9a86bcc6bbce3ef8f03c360b581f047347
DESKTOP-0JCUD6O\asus <diwserala@gmail.com>
2022-10-22 08:04:58 +0545

    Spring student repo

---
+++ src/main/java/com/example/studentdata/model/Student.java
@@ -0,0 +1,23 @@
+package com.example.studentdata.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String address;
+    private int rollNo;
+}
